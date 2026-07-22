e304fb5b7f6fe85afd529025497acdec6125f7c9
AbhayPrasad123 <prasadabhay03@gmail.com>
2026-07-22 16:40:07 +0530

    code to generate dag with cmd

---
+++ Airflow/DagGenerator/create_dag.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from jinja2 import Environment, FileSystemLoader
+
+
+dag_name = sys.argv[1]
+
+
+env = Environment(
+    loader=FileSystemLoader("templates")
+)
+
+template = env.get_template("dag_template.j2")
+
+
+output = template.render(
+    dag_name=dag_name
+)
+
+
+output_dir = "../dags/generated"
+
+os.makedirs(output_dir, exist_ok=True)
+
+output_file = os.path.join(
+    output_dir,
+    f"{dag_name}.py"
+)
+
+with open(output_file, "w") as f:
+    f.write(output)
+
+print("=" * 40)
+print(f"DAG Created : {dag_name}")
+print(f"Saved at : {output_file}")
+print("=" * 40)
